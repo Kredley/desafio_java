e9f1b76bad11996d107252bdfc75ea08741a6714
Kredley <kdynm1988@hotmail.com>
2016-05-31 15:19:38 -0300

    Alteração da chave do CadastroPhone para Long no CadastroPhoneRepository

--- src/main/java/com/concretesolutions/CadastroPhoneRepository.java
+++ src/main/java/com/concretesolutions/CadastroPhoneRepository.java
@@ -3,6 +3,6 @@ package com.concretesolutions;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 @Repository
-public interface CadastroPhoneRepository extends CrudRepository <CadastroPhone, String>{
+public interface CadastroPhoneRepository extends CrudRepository <CadastroPhone, Long>{
 
 }
